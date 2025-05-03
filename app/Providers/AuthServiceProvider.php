4fb865d9c2c0f53cb5d1f7119484f0e2c53430a6
Tu Nombre <tuemail@example.com>
2025-05-03 17:43:01 -0600

    Register AnimePolicy for authorization in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+Use App\Models\Anime;
+Use App\Policies\AnimePolicy;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -14,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //
+        Anime::class => AnimePolicy::class,
     ];
 
     /**
@@ -22,5 +25,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->registerPolicies();
     }
 }
