17f60b9e71d9070bef81e5554d117d031aaaa8a8
Tu Nombre <tuemail@example.com>
2025-05-03 18:21:39 -0600

    Add user_roles migration and seeders for role-based access control

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $adminRole = Role::where('name', 'admin')->first();
+        $editorRole = Role::where('name', 'editor')->first();
+        $viewerRole = Role::where('name', 'viewer')->first();
+
+        $adminUser = User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('$WhiteUsagiAdmin#1!'),
+        ]);
+        $adminUser->roles()->attach($adminRole);
+
+        $editorUser = User::create([
+            'name' => 'Editor User',
+            'email' => 'editor@example.com',
+            'password' => Hash::make('$WhiteUsagiEditor#1!'),
+        ]);
+        $editorUser->roles()->attach($editorRole);
+
+        $viewerUser = User::create([
+            'name' => 'Viewer User',
+            'email' => 'viewer@example.com',
+            'password' => Hash::make('$WhiteUsagiViewer#1!'),
+        ]);
+        $viewerUser->roles()->attach($viewerRole);
+
+
+
+    }
+}
