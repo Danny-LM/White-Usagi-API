937c8fae5e11b83f8d3fd90ac5ab47a939bcfac7
Tu Nombre <tuemail@example.com>
2025-05-03 20:31:06 -0600

    Add placeholder docblocks to model and policy methods for better documentation

--- app/Policies/GenrePolicy.php
+++ app/Policies/GenrePolicy.php
@@ -10,36 +10,57 @@ class GenrePolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * 
+     */
     public function viewAny(User $user)
     {
         return true;
     }
 
+    /**
+     * 
+     */
     public function view(User $user, Genre $genre)
     {
         return true;
     }
 
+    /**
+     * 
+     */
     public function create(User $user)
     {
         return $user->roles()->where('name', 'editor')->exists() || $user->roles()->where('name', 'admin')->exists();
     }
 
+    /**
+     * 
+     */
     public function update(User $user, Genre $genre)
     {
         return $user->roles()->where('name', 'editor')->exists() || $user->roles()->where('name', 'admin')->exists();
     }
 
+    /**
+     * 
+     */
     public function delete(User $user, Genre $genre)
     {
         return $user->roles()->where('name', 'admin')->exists(); // Maybe only admins can delete genres
     }
 
+    /**
+     * 
+     */
     public function restore(User $user, Genre $genre)
     {
         return false;
     }
 
+    /**
+     * 
+     */
     public function forceDelete(User $user, Genre $genre)
     {
         return false;
--- app/Policies/StudioPolicy.php
+++ app/Policies/StudioPolicy.php
@@ -10,36 +10,57 @@ class StudioPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * 
+     */
     public function viewAny(User $user)
     {
         return true;
     }
 
+    /**
+     * 
+     */
     public function view(User $user, Studio $studio)
     {
         return true;
     }
 
+    /**
+     * 
+     */
     public function create(User $user)
     {
         return $user->roles()->where('name', 'editor')->exists() || $user->roles()->where('name', 'admin')->exists();
     }
 
+    /**
+     * 
+     */
     public function update(User $user, Studio $studio)
     {
         return $user->roles()->where('name', 'editor')->exists() || $user->roles()->where('name', 'admin')->exists();
     }
 
+    /**
+     * 
+     */
     public function delete(User $user, Studio $studio)
     {
         return $user->roles()->where('name', 'admin')->exists(); // Maybe only admins can delete studios
     }
 
+    /**
+     * 
+     */
     public function restore(User $user, Studio $studio)
     {
         return false;
     }
 
+    /**
+     * 
+     */
     public function forceDelete(User $user, Studio $studio)
     {
         return false;
